24d775fe5ba8bff1af46c54efc3a0cf031ee4b2d
rupeshtiwari <roopkt@gmail.com>
2021-06-02 08:01:07 -0400

    first commit

---
+++ Controllers/SalaryController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+
+namespace WebApp_OpenIDConnect_DotNet.Controllers
+{
+    [EnableCors("CorsPolicy")]
+    [Authorize(Roles = "Business")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalaryController : ControllerBase
+    {
+        // GET: api/<SalaryController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SalaryController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SalaryController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SalaryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SalaryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(() => {
+    $('#getValues').on('click', () => {
+        $.get('https://localhost:44321/api/salary').then((s) => {
+            $('#values').text(s);
+
+        }).catch(e => {
+            $('#values').text(e.statusText);
+        });
+    });
+});
